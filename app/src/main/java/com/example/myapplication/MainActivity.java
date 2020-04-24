11758196c8d4bdb68e9fd3eafdbab8fb76021623
Asif67 <asif.nwu@gmail.com>
2020-04-24 11:33:32 +0600

    Pre-requisite complete

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mParaLabel;
+    private FingerprintManager fingerprintManager;
+    private KeyguardManager keyguardManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mParaLabel = (TextView)findViewById(R.id.paraLabel);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            fingerprintManager = (FingerprintManager) getSystemService(FINGERPRINT_SERVICE);
+            keyguardManager = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+            if(!fingerprintManager.isHardwareDetected()){
+                mParaLabel.setText("N");
+            }
+            else if(ContextCompat.checkSelfPermission(this, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
+                mParaLabel.setText("P");
+            }
+            else if(!keyguardManager.isKeyguardSecure()){
+                mParaLabel.setText("S");
+            }
+            else if(!fingerprintManager.hasEnrolledFingerprints()){
+                mParaLabel.setText("F");
+            }
+            else{
+                mParaLabel.setText("A");
+            }
+        }
     }
 }
